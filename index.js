857c334df9ec19e4a6faa824a602afeaf29c8fcc
Angel-Davila <a6519150005@utch.edu.mx>
2021-08-11 04:10:08 -0600

    base file changed, spaces deleted

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import 'react-native-gesture-handler'
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import { Linking } from 'react-native'
 
 AppRegistry.registerComponent(appName, () => App);
 
